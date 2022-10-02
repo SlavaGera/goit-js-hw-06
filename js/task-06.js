8e8aeba488996828a01d8219104692ce8420ce6d
Slavik Herasiuta <theprodigy77@gmail.com>
2022-10-02 22:28:33 +0300

    update-fix-task-6

--- js/task-06.js
+++ js/task-06.js
@@ -2,12 +2,11 @@ const inputRef = document.querySelector('#validation-input');
 console.log(inputRef.getAttribute('data-length'));
 
 inputRef.addEventListener('blur', event => {
-    if (event.target.value.length < inputRef.getAttribute('data-length')) {
-        inputRef.classList.remove('valid');
-        inputRef.classList.add('invalid');
-    } else {
+    if (Number(event.currentTarget.value.length) === Number(inputRef.getAttribute('data-length'))) {
         inputRef.classList.remove('invalid');
         inputRef.classList.add('valid');
+    } else {
+        inputRef.classList.remove('valid');
+        inputRef.classList.add('invalid');
     }
 })
-
